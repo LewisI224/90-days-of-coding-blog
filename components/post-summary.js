6f22590d6f4ace9384c9082dbe2ecfc4e20e8478
Lewis Inches <incheslewis@gmail.com>
2022-06-06 19:58:17 +0100

    cleanup

--- components/post-summary.js
+++ components/post-summary.js
@@ -2,49 +2,52 @@ import Link from 'next/link';
 import Image from 'next/image'
 import Date from './date';
 
+import styles from './css-modules/post-summary.module.css'
+
 export default function PostSummary ({id, title, subtitle, date, imgurl}) {
 
     return (
         <div className="col">
             
         <div >
+            <Link href={`/posts/${id}`}>
+                <a className={styles.link}>
 
-            <div className='card mb-3' key={id}>
-                <h5 className="card-header"><Date dateString={date} /></h5>
-                {imgurl ? (
-                    <>
-                    <Image 
-                    src={`/images/${imgurl}.png`}
-                    alt="Blog Post Image"
-                    width="960px"
-                    height="480px"
-                    layout='responsive'
-                    />
-                    </>
-                ) : (
-                    <>
-                    <Image 
-                    src={`/images/default.png`}
-                    alt="Blog Post Image"
-                    width="960px"
-                    height="480px"
-                    layout='responsive'
-                    />
-                    </>
-                )}
-                    
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className='card-text'>{subtitle}</p>
+                    <div className='card mb-3' key={id}>
+                        
+                        <h5 className="card-header"><Date dateString={date} /></h5>
+                        {imgurl ? (
+                            <>
+                            <Image 
+                            src={`/images/${imgurl}.png`}
+                            alt="Blog Post Image"
+                            width="960px"
+                            height="480px"
+                            layout='responsive'
+                            />
+                            </>
+                        ) : (
+                            <>
+                            <Image 
+                            src={`/images/default.png`}
+                            alt="Blog Post Image"
+                            width="960px"
+                            height="480px"
+                            layout='responsive'
+                            />
+                            </>
+                        )}
+                            
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className='card-text'>{subtitle}</p>
+                        </div>
 
-                    <Link href={`/posts/${id}`}>
-                        <a className="btn btn-primary">Go To Post</a>
-                    </Link>
-                </div>
-                
-                
+                    </div>
+                </a>
+            </Link>
 
-            </div>
+            
 
         </div>
 
